30e568eecc5c33fc7298e3de2b0cd3df6f9d410f
abdulahad-se <abdulahad17p@gmail.com>
2026-07-12 15:13:53 +0500

    Adding Lab 3 Tasks

--- 02_Labs/C_JavaLab.java
+++ 02_Labs/C_JavaLab.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class C_JavaLab {
-    // Task 1
+    // Task 1.
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the hour (0-23): ");
