75fb393d5af3031ead701422147630734d3708b4
abdulahad-se <abdulahad17p@gmail.com>
2026-07-27 19:12:57 +0500

    VaraiblesAndDatatypes

---
+++ 01_Basics/B_VariablesandDatatypes.java
@@ -0,0 +1,16 @@
+public class B_VariablesandDatatypes {
+    public static void main(String[] args) {
+        String s = "Hey welcome to Java learning";
+        byte a = 112;
+        int a1 = 1111111111;
+        long a2 = 1111111111111111L;
+        float a3 = 123.2f;
+        double a4 = 12.23455;
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+    }
+}
