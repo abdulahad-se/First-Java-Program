1c0a91180835a963bbce6fc02100a8eb9cc3b303
abdulahad-se <abdulahad17p@gmail.com>
2026-07-27 19:30:18 +0500

    The file firstjavacode and threevariablecal is deleted

--- 01_Basics/firstjavacode.java
+++
@@ -1,5 +0,0 @@
-public class firstjavacode {
-  public static void main(String[] args) {
-    System.out.println("hello world from Java Developer:");
-  }
-}
--- 01_Basics/threevariablecal.java
+++
@@ -1,9 +0,0 @@
-public class threevariablecal {
-    public static void main(String[] args) {
-        int a = 10;
-        long b = 20;
-        double c = 9.0;
-        double result = (a + b) / c;
-        System.out.println("the result is::" + result);
-    }
-}
